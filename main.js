d672e4eafd5bc8fc28e939002889168127878a95
Adam-Gleeson <98527173+Adam-Gleeson@users.noreply.github.com>
2022-10-03 23:22:24 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+function changeAll()
+{
+    let myH1 = null;
+    let myDiv = null;
+    let myImg = null;
+    let myImg2 = null;
+
+    myH1 = document.getElementById("header")
+
+    myH1.style.color = "green"
+    myH1.style.fontSize = "0"
+
+    myDiv = document.getElementById("myDiv")
+
+    myDiv.style.color = "red"
+    myDiv.style.fontSize = "200px"
+
+  myImg = document.getElementById("myImg")
+
+  myImg.style.height = "100%"
+  myImg.style.width = "100%"
+
+  myImg2 = document.getElementById("myImg2")
+
+  myImg2.style.height = "0"
+  myImg2.style.width = "0"
+    
+}
+function changeIt()
+{
+    let myH1 = null;
+    let myImg3 = null;
+    let myImg2 = null;
+
+    myH1 = document.getElementById("header")
+
+    myH1.style.color = "green"
+    myH1.style.fontSize = "0"
+
+    myDiv2 = document.getElementById("myDiv2")
+
+    myDiv2.style.color = "red"
+    myDiv2.style.fontSize = "200px"
+
+  myImg3 = document.getElementById("myImg3")
+
+  myImg3.style.height = "100%"
+  myImg3.style.width = "100%"
+
+  myImg2 = document.getElementById("myImg2")
+
+  myImg2.style.height = "0"
+  myImg2.style.width = "0"
+}
