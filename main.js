a9ea068dba2bd502047f928ae0b7d6bb69e3db58
Adam-Gleeson <98527173+Adam-Gleeson@users.noreply.github.com>
2022-10-04 23:42:02 +0100

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-function change(joeBiden, arabJoeBiden){ 
-    joeBiden.style.cssText = "color: red;  font-size: 200px";
-    arabJoeBiden.style.cssText = "height: 100%;  width: 100%";
-      
-    for(let i = 1; i < 5; i++) {
-        document.getElementById("Joe"+i).remove();
-    }
-     }
