27b5edb899397302f6d24dcdc6cec8cc3a7c7edc
Tim <91697051+Timeonas@users.noreply.github.com>
2022-10-06 00:09:17 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,40 @@
 
+  
+let Balls = 0;
+
 function change(joeBiden, arabJoeBiden){
 
-  joeBiden.style.cssText = "color: red;  font-size: 200px";
+  if(joeBiden == myDiv2){
+    Balls=1;
+  }
+
+  if (Balls == 1){
+  joeBiden.style.cssText = "color: red;  font-size: 165px";
   arabJoeBiden.style.cssText = "height: 100%;  width: 100%";
 
-  for(let i = 1; i < 5; i++) 
+  for(let i = 1; i < 7; i++) 
   {
     document.getElementById("Joe"+i).remove();
   } 
-  
-  document.getElementById("audio").play();
+
+  document.getElementById("CarPic").remove();
+  document.getElementById("Fat").remove();
+}
+else 
+alert("Stop right there, how are you planning on drink driving? Without a car?")
 }
 
 function selectOption(){
-    var Choice = document.getElementById("Car").value;
+    var Choice = document.getElementById("Joe5").value;
+    if(Choice==0)
+      Balls = 0;
+    else
     document.getElementById("CarPic").style ="background-image: url('Images/Car"+Choice+".jpg');"
+    Balls = 1;
+}
+
+function playAudio(Audio){
+  if(Balls==1){
+    document.getElementById("audio"+Audio).play();
+  }
 }
